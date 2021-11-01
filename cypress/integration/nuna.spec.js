f630efab74d8d94cf57f339c7ac894c956240f7b
Vitaly <universallly@gmail.com>
2021-10-31 20:10:56 -0400

    Moved data out of fixtures to use it in beforeeach Added Viewport Some cleanup

--- cypress/integration/nuna.spec.js
+++ cypress/integration/nuna.spec.js
@@ -1,39 +1,40 @@
 import Nuna from './PageObjects/nuna';
 Cypress.on('uncaught:exception', (err, runnable) => false );
 
-describe('Google Translate tests', function(){
+describe('Google Translate tests', () => {
     const translate = new Nuna();
+    const data = require('../fixtures/translate.json');
 
-    beforeEach(function () {
-        cy.fixture( 'translate.json' ).as( 'data' );
+    before( () => {
+        cy.viewport( 1920, 1080 );
+        Cypress.config( 'viewportWidth', 1920 );
+        Cypress.config( 'viewportHeight', 1080 );
+    });
+
+    beforeEach( () => {
         cy.visit( '/' );
-    })
+        cy.title().should( 'eq', data.title );
+        translate.chooseLanguages( data.words.sourceLang, data.words.transLang );
+        translate.typeText( data.words.sourceWord );
+    });
 
-    it('Translates fron German to Spanish', function(){
-        cy.title().should( 'eq', this.data.title );
-        translate.chooseLanguages( this.data.words.sourceLang, this.data.words.transLang );
-        translate.typeText( this.data.words.sourceWord );
-        translate.outputArea( this.data.words.transWord ).should( 'be.visible' );
-        
-    })
-    it('Swaps the Languages', function(){
-        cy.title().should( 'eq', this.data.title );
-        translate.chooseLanguages( this.data.words.sourceLang, this.data.words.transLang );
-        translate.typeText( this.data.words.sourceWord );
+    it('Translates fron German to Spanish', () => {
+        translate.outputArea( data.words.transWord ).should( 'be.visible' );
+    });
+
+    it('Swaps the Languages', () => {
         cy.wait( 500 );
         translate.swapLangBtn().click();
         cy.wait( 500 );
-        translate.outputArea( this.data.words.sourceWord ).should( 'be.visible' );
-    })
-    it('Using screen keyboard', function(){
-        cy.title().should( 'eq', this.data.title );
-        translate.chooseLanguages( this.data.words.sourceLang, this.data.words.transLang );
-        translate.typeText( this.data.words.sourceWord );
+        translate.outputArea( data.words.sourceWord ).should( 'be.visible' );
+    });
+
+    it('Using screen keyboard', () => {
         translate.swapLangBtn().click();
         translate.inputArea().clear();
         translate.virtKeyboardBtn().click();
-        translate.virtKeyboardKeyClick( this.data.words.keyboardWordCodes );
-        cy.wait( 500 );
-        translate.outputArea( this.data.words.keyboardWord ).should( 'be.visible' );
-    })
+        translate.virtKeyboardKeyClick( data.words.keyboardWordCodes );
+        cy.wait( 1000 ); // to solve the error
+        translate.outputArea( data.words.keyboardWord ).should( 'be.visible' );
+    });
 });
